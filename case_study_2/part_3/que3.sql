83446a86481cab8aa2eea8849181865e122352f7
Harsh <hb755037@gmail.com>
2026-01-26 17:24:29 +0530

    Day 15 completed 2 questions

--- case_study_2/part_3/que3.sql
+++ case_study_2/part_3/que3.sql
@@ -1,2 +1,31 @@
 -- 3. What was the most common exclusion?
 
+WITH exclusion_split AS (
+	select 
+		TRY_CAST(rtrim(LTRIM(s.value)) AS INT) as topping_id
+	from customer_orders co
+	inner join runner_orders ro
+		on co.order_id = ro.order_id
+	cross apply string_split(co.exclusions, ',') s 
+	WHERE ro.pickup_time IS NOT NULL
+	AND ro.pickup_time <> 'null'
+	AND (ro.cancellation IS NULL 
+		OR ro.cancellation = '' 
+		OR ro.cancellation = 'null') and 
+		co.exclusions <> 'null' and co.exclusions is not null 
+	and co.exclusions <> ''
+)
+select TOP 1 
+	pt.topping_name, 
+	COUNT(*) as times_added
+from exclusion_split es 
+inner join pizza_toppings pt
+	on es.topping_id = pt.topping_id
+group by pt.topping_name
+order by times_added desc
+
+-- output:-
+| topping_name | times_added |
+| ------------ | ----------- |
+| Cheese       | 3           |
+
---
+++ case_study_2/part_3/que4.sql
@@ -0,0 +1,6 @@
+-- 4. Generate an order item for each record in the customers_orders table in the format of one of the following:
+--     Meat Lovers
+--     Meat Lovers - Exclude Beef
+--     Meat Lovers - Extra Bacon
+--     Meat Lovers - Exclude Cheese, Bacon - Extra Mushroom, Peppers
+
